14c9b4404da8b099a9ac02862d647b1c89e06e8a
michael <bright.tiger@gmail.com>
2021-03-07 20:22:19 -0500

    cleanup

--- solinear/graph.sh
+++ solinear/graph.sh
@@ -6,7 +6,7 @@ VARS='test-vars.sh'
 
 if [ -f "${VARS}" ]; then
   source test-vars.sh
-  ../at-graph.py $@ -d=${PREFIX}-output -m=graph -p=${PREFIX} -t="$TITLE"
+  ../at-graph.py $@ -d=${PREFIX}-u3-data -m=graph -p=${PREFIX} -t="$TITLE"
 else
   echo "*** vars file '${VARS}' not found - run ./test.sh to configure!"
 fi
